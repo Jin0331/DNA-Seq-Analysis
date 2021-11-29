30fb7e9ed9c2f917d87751b4427678d41b2d3908
Jin0331 <M18525@hallym.ac.kr>
2021-11-29 11:22:02 +0900

    germline variant update

--- Germline_variant_discovery/align_ref.sh
+++ Germline_variant_discovery/align_ref.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# docker run -dit -v ${PWD}:/data --name samtool staphb/samtools:1.11 bash
+# apt update && apt install bwa
+
 while getopts f:w:r: flag
 do
     case "${flag}" in
@@ -12,10 +15,9 @@ done
 for i in ${fastqFolder}/*_1.fq.gz
 do
  filename=$(basename $i _1.fq.gz)
+ 
  mapFile=${workPath}/${filename}_bwa.bam
  mapped[${#mapped[*]}]=$mapFile
-
- bwa mem -t 160 -Ma \
- -R '@RG\\tID:${filename}\\tSM:${filename}\\tPL:ILM\\tLB:${filename}' \ $ref $i ${i%1.fq.gz}2.fq.gz \
- | samtools sort - -@ 40 -n -m 4G -T ${i%R1.fq.gz} -o $mapFile
+ bwa mem -t 15 -Ma -R @RG\\tID:${filename}\\tSM:${filename}\\tPL:ILM\\tLB:${filename} $ref $i ${i%1.fq.gz}2.fq.gz \
+ |samtools sort - -@ 15 -n -m 10G -T ${i%R1.fastq.gz} -o $mapFile
 done
