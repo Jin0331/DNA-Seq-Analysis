8663961e17ff16802cbc472dddd1946dbdaecb69
Jin0331 <M18525@hallym.ac.kr>
2021-11-30 09:15:04 +0900

    update

--- Pre-Alignment/Mardup.sh
+++ Pre-Alignment/Mardup.sh
@@ -14,34 +14,61 @@ done
 
 # Using GATK
 for mapFile in ${bamFolder}/*_bwa_dedup.bam
-do
-    for i in `seq -f %04g 0 14`
-    do  
-        filename=$(basename $mapFile _dedup.bam)
-        outfile=${filename}_dedup_recal_data_${i}.table
-        gatk --java-options "-Xmx4G -XX:+UseParallelGC \
-                            -XX:ParallelGCThreads=4" BaseRecalibrator \
-                            -L ${interval}/${i}-scattered.interval_list \
-                            -R $ref \
-                            -I ${mapFile} \
-                            --known-sites $dbsnp \
-                            -O ${workPath}/${outfile} &
-    done
-    wait
+do  
+    # # BaseRecalibrator
+    # for i in `seq -f %04g 0 14`
+    # do  
+    #     filename=$(basename $mapFile _dedup.bam)
+    #     outfile=${filename}_dedup_recal_data_${i}.table
+    #     gatk --java-options "-Xmx4G -XX:+UseParallelGC \
+    #                         -XX:ParallelGCThreads=4" BaseRecalibrator \
+    #                         -L ${interval}/${i}-scattered.interval_list \
+    #                         -R $ref \
+    #                         -I ${mapFile} \
+    #                         --known-sites $dbsnp \
+    #                         -O ${workPath}/${outfile} &
+    # done
+    # wait
+
+    # ApplyBQSR
     for i in `seq -f %04g 0 14`
     do
-        filename=$(basename $mapFile _dedup.bam)
+        filename=$(basename $mapFile _dedup.bam)       
         bqfile=${workPath}/${filename}_dedup_recal_data_$i.table
         output=${filename}_dedup_recal_$i.bam
-        gatk --java-options "-Xmx4G -Xmx4G -XX:+UseParallelGC \
-             -XX:ParallelGCThreads=4" ApplyBQSR -R $ref \
-        -I ${mapFile} \
-        -L ${interval}/${i}-scattered.interval_list \
-        -bqsr $bqfile \
-        --static-quantized-quals 10 \
-        --static-quantized-quals 20 \
-        --static-quantized-quals 30  \
-        -O ${workPath}/${output} &
+
+        # file list
+        if [ ${i} = 1 ]
+        then
+            echo ${workPath}/${output} > ${workPath}/${filename}_file.list
+        else
+            echo ${workPath}/${output} >> ${workPath}/${filename}_file.list
+        fi
+
+        # gatk --java-options "-Xmx4G -Xmx4G -XX:+UseParallelGC \
+        #      -XX:ParallelGCThreads=4" ApplyBQSR -R $ref \
+        #                     -I ${mapFile} \
+        #                     -L ${interval}/${i}-scattered.interval_list \
+        #                     -bqsr $bqfile \
+        #                     --static-quantized-quals 10 \
+        #                     --static-quantized-quals 20 \
+        #                     --static-quantized-quals 30  \
+        #                     -O ${workPath}/${output} &
     done
     wait
+
+    # GatherBamfile & Sortsam
+    filename=$(basename $mapFile _dedup.bam)
+    gatk GatherBamFiles -I ${workPath}/${filename}_file.list \
+                        -O ${workPath}/${filename}_unsorted.bam \
+                        -R ${ref}
+
+    gatk SortSam -I ${workPath}/${filename}_unsorted.bam \
+                 -O ${workPath}/${filename}_final.bam \
+                 --SORT_ORDER coordinate -VALIDATION_STRINGENCY LENIENT
+
+    gatk BuildBamIndex -I ${workPath}/${filename}_final.bam \
+                       -O ${workPath}/${filename}_final.bai \
+                       -VALIDATION_STRINGENCY LENIENT
+
 done
