6f4f20f74b0972205d498e145f5c1cce127030e0
Jin0331 <M18525@hallym.ac.kr>
2021-11-30 16:29:42 +0900

    update

--- Pre-Alignment/Mardup.sh
+++ Pre-Alignment/Mardup.sh
@@ -23,14 +23,14 @@ do
     # BaseRecalibrator
     for i in `seq -f %04g 0 14`
     do  
-        filename=$(basename $mapFile _dedup.bam)
+        filename=$(basename ${mapFile} _dedup.bam)
         outfile=${filename}_dedup_recal_data_${i}.table
         gatk --java-options "-Xmx4G -XX:+UseParallelGC \
                             -XX:ParallelGCThreads=4" BaseRecalibrator \
                             -L ${interval}/${i}-scattered.interval_list \
-                            -R $ref \
+                            -R ${ref} \
                             -I ${mapFile} \
-                            --known-sites $dbsnp \
+                            --known-sites ${dbsnp} \
                             -O ${workPath}/${outfile} &
     done
     wait
@@ -38,9 +38,9 @@ do
     # ApplyBQSR
     for i in `seq -f %04g 0 14`
     do
-        filename=$(basename $mapFile _dedup.bam)       
-        bqfile=${workPath}/${filename}_dedup_recal_data_$i.table
-        output=${filename}_dedup_recal_$i.bam
+        filename=$(basename ${mapFile} _dedup.bam)       
+        bqfile=${workPath}/${filename}_dedup_recal_data_${i}.table
+        output=${filename}_dedup_recal_${i}.bam
 
         # file list
         if [ ${i} = 1 ]
@@ -50,11 +50,11 @@ do
             echo ${workPath}/${output} >> ${workPath}/${filename}_file.list
         fi
 
-        gatk --java-options "-Xmx4G -Xmx4G -XX:+UseParallelGC \
-             -XX:ParallelGCThreads=4" ApplyBQSR -R $ref \
+        gatk --java-options "-Xmx8G -Xmx8G -XX:+UseParallelGC \
+             -XX:ParallelGCThreads=4" ApplyBQSR -R ${ref} \
                             -I ${mapFile} \
                             -L ${interval}/${i}-scattered.interval_list \
-                            -bqsr $bqfile \
+                            -bqsr ${bqfile} \
                             --static-quantized-quals 10 \
                             --static-quantized-quals 20 \
                             --static-quantized-quals 30  \
@@ -68,7 +68,7 @@ do
         # GatherBamfile & Sortsam
         mkdir -p ${finalPath}
 
-        filename=$(basename $mapFile _dedup.bam)
+        filename=$(basename ${mapFile} _dedup.bam)
         gatk GatherBamFiles -I ${workPath}/${filename}_file.list \
                             -O ${workPath}/${filename}_unsorted.bam \
                             -R ${ref}
@@ -81,9 +81,6 @@ do
                         -O ${finalPath}/${filename}_final.bai \
                         -VALIDATION_STRINGENCY LENIENT
     fi
-
-
-
 done
 
 if [ ${type} = "somatic" ]
