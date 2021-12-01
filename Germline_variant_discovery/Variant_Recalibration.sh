cefa002b34c590df1b64edc62ed83514a1e01662
Jin0331 <M18525@hallym.ac.kr>
2021-12-02 08:11:11 +0900

    update

--- Germline_variant_discovery/Variant_Recalibration.sh
+++ Germline_variant_discovery/Variant_Recalibration.sh
@@ -15,6 +15,7 @@ do
     esac
 done
 
+# directory
 mkdir -p ${workvqsr}
 mkdir -p ${finalPath}
 
@@ -25,11 +26,11 @@ gatk --java-options "-Xms4G -Xmx4G -XX:ParallelGCThreads=5" VariantRecalibrator 
   -V ${mergevcf} \
   -O ${workvqsr}/recalibrate_SNP.recal \
   -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR \
-  --tranches-file output_SNP1.tranches \
   -tranche 100.0 -tranche 99.95 -tranche 99.9 \
   -tranche 99.5 -tranche 99.0 -tranche 97.0 -tranche 96.0 \
   -tranche 95.0 -tranche 94.0 -tranche 93.5 -tranche 93.0 \
   -tranche 92.0 -tranche 91.0 -tranche 90.0 \
+  --tranches-file ${workvqsr}/recalibrate_SNP.tranches \
   --resource:hapmap,known=false,training=true,truth=true,prior=15.0 ${hapmap} \
   --resource:omni,known=false,training=true,truth=false,prior=12.0 ${omni} \
   --resource:1000G,known=false,training=true,truth=false,prior=10.0 ${kg1000} \
@@ -37,17 +38,37 @@ gatk --java-options "-Xms4G -Xmx4G -XX:ParallelGCThreads=5" VariantRecalibrator 
   --rscript-file ${workvqsr}/output_SNP1.plots.R
 
 # Variant quality score recalibration - INDELs
-gatk --java-options "-Xmx25G -XX:ParallelGCThreads=5" VariantRecalibrator \
+gatk --java-options "-Xms4G -Xmx4G -XX:ParallelGCThreads=5" VariantRecalibrator \
   -mode INDEL \
   -R ${ref} \
   -V ${mergevcf} \
-  -O ${workvqsr}/merged_indel1.recal \
+  -O ${workvqsr}/recalibrate_INDEL.recal \
   -an QD -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP \
   -tranche 100.0 -tranche 99.95 -tranche 99.9 \
   -tranche 99.5 -tranche 99.0 -tranche 97.0 -tranche 96.0 \
   -tranche 95.0 -tranche 94.0 -tranche 93.5 -tranche 93.0 \
   -tranche 92.0 -tranche 91.0 -tranche 90.0 \
-  --tranches-file output_indel1.tranches \
+  --tranches-file ${workvqsr}/recalibrate_INDEL.tranches \
   --resource:mills,known=false,training=true,truth=true,prior=12.0 ${mills} \
   --resource:dbsnp,known=true,training=false,truth=false,prior=2.0 ${dbsnp} \
   --rscript-file ${workvqsr}/output_indel1.plots.R
+
+  # Apply recalibration to SNPs
+gatk --java-options "-Xms4G -Xmx4G -XX:ParallelGCThreads=5" ApplyVQSR \
+  -V ${mergevcf} \
+  -O ${workvqsr}/recalibrated_snps_raw_indels.vcf \
+  --recal-file ${workvqsr}/recalibrate_SNP.recal \
+  --tranches-file ${workvqsr}/recalibrate_SNP.tranches \
+  -truth-sensitivity-filter-level 99.5 \
+  --create-output-variant-index true \
+  -mode SNP
+
+  # Apply recalibration to Indels
+gatk --java-options "-Xms4G -Xmx4G -XX:ParallelGCThreads=5" ApplyVQSR \
+  -V ${workvqsr}/recalibrated_snps_raw_indels.vcf \
+  -O ${finalPath}/merged_snp_indel.recal.vcf 
+  --recal-file ${workvqsr}/recalibrate_INDEL.recal \
+  --tranches-file ${workvqsr}/recalibrate_INDEL.tranches \
+  -truth-sensitivity-filter-level 99.0 \
+  --create-output-variant-index true
+  -mode INDEL
