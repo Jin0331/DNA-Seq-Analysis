b05209e7032873928bd060f8cfeae9215830e89c
Jin0331 <M18525@hallym.ac.kr>
2021-12-01 16:59:32 +0900

    update

--- Germline_variant_discovery/Variant_Recalibration.sh
+++ Germline_variant_discovery/Variant_Recalibration.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-while getopts v:r:b:h:o:d:i:o:t: flag
+while getopts v:r:b:h:n:k:d:m:o: flag
 do
     case "${flag}" in
         v) mergevcf=${OPTARG};;
         r) ref=${OPTARG};;
         b) workvqsr=${OPTARG};;
         h) hapmap=${OPTARG};;
-        o) omni=${OPTARG};;
+        n) omni=${OPTARG};;
+        k) kg1000=${OPTARG};;
         d) dbsnp=${OPTARG};;
-
-        i) interval=${OPTARG};;
+        m) mills=${OPTARG};;
         o) finalPath=${OPTARG};;
-        t) type=${OPTARG};;
     esac
 done
 
 mkdir -p ${workvqsr}
+mkdir -p ${finalPath}
 
 # Variant quality score recalibration - SNPs
-gatk --java-options " -Xms4G -Xmx4G -XX:ParallelGCThreads=5" VariantRecalibrator \
+gatk --java-options "-Xms4G -Xmx4G -XX:ParallelGCThreads=5" VariantRecalibrator \
   -mode SNP \
   -R ${ref} \
   -V ${mergevcf} \
   -O ${workvqsr}/recalibrate_SNP.recal \
   -an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR \
-  --maxgaussians 6 --tranches-file output_SNP1.tranches \
+  --tranches-file output_SNP1.tranches \
   -tranche 100.0 -tranche 99.95 -tranche 99.9 \
   -tranche 99.5 -tranche 99.0 -tranche 97.0 -tranche 96.0 \
   -tranche 95.0 -tranche 94.0 -tranche 93.5 -tranche 93.0 \
@@ -33,21 +33,21 @@ gatk --java-options " -Xms4G -Xmx4G -XX:ParallelGCThreads=5" VariantRecalibrator
   --resource:hapmap,known=false,training=true,truth=true,prior=15.0 ${hapmap} \
   --resource:omni,known=false,training=true,truth=false,prior=12.0 ${omni} \
   --resource:1000G,known=false,training=true,truth=false,prior=10.0 ${kg1000} \
-  --resource:dbsnp,known=true,training=false,truth=false,prior=7.0 ${dbsnp}] \
-  --rscript-file output_SNP1.plots.R
+  --resource:dbsnp,known=true,training=false,truth=false,prior=7.0 ${dbsnp} \
+  --rscript-file ${workvqsr}/output_SNP1.plots.R
 
 # Variant quality score recalibration - INDELs
-gatk --java-options "-Djava.io.tmpdir=/lscratch/$SLURM_JOBID -Xms4G -Xmx4G -XX:ParallelGCThreads=2" VariantRecalibrator \
+gatk --java-options "-Xmx25G -XX:ParallelGCThreads=5" VariantRecalibrator \
+  -mode INDEL \
+  -R ${ref} \
+  -V ${mergevcf} \
+  -O ${workvqsr}/merged_indel1.recal \
+  -an QD -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP \
   -tranche 100.0 -tranche 99.95 -tranche 99.9 \
   -tranche 99.5 -tranche 99.0 -tranche 97.0 -tranche 96.0 \
   -tranche 95.0 -tranche 94.0 -tranche 93.5 -tranche 93.0 \
   -tranche 92.0 -tranche 91.0 -tranche 90.0 \
-  -R /fdb/igenomes/Homo_sapiens/UCSC/hg38/Sequence/WholeGenomeFasta/genome.fa \
-  -V merged.vcf.gz \
-  --resource:mills,known=false,training=true,truth=true,prior=12.0 \
-  /fdb/GATK_resource_bundle/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz \
-  --resource:dbsnp,known=true,training=false,truth=false,prior=2.0 \
-  /fdb/GATK_resource_bundle/hg38/dbsnp_146.hg38.vcf.gz \
-  -an QD -an MQRankSum -an ReadPosRankSum -an FS -an SOR -an DP \
-  -mode INDEL -O merged_indel1.recal --tranches-file output_indel1.tranches \
-  --rscript-file output_indel1.plots.R
+  --tranches-file output_indel1.tranches \
+  --resource:mills,known=false,training=true,truth=true,prior=12.0 ${mills} \
+  --resource:dbsnp,known=true,training=false,truth=false,prior=2.0 ${dbsnp} \
+  --rscript-file ${workvqsr}/output_indel1.plots.R
