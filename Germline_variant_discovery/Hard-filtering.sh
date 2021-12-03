2ba31de7f912a96642797c7b875631614ec91bb4
Jin0331 <M18525@hallym.ac.kr>
2021-12-03 15:36:16 +0900

    update

--- Germline_variant_discovery/Hard-filtering.sh
+++ Germline_variant_discovery/Hard-filtering.sh
@@ -12,52 +12,52 @@ do
     esac
 done
 
-# # directory
-# mkdir -p ${workhard}
-# mkdir -p ${finalPath}
+# directory
+mkdir -p ${workhard}
+mkdir -p ${finalPath}
 
-# # SPLIT SNP
-# gatk --java-options '-Xmx25g' SelectVariants \
-#  -R ${ref} \
-#  -V ${mergevcf} \
-#  -select-type SNP \
-#  -O ${workhard}/Exome_Norm_HC_calls.snps.vcf
+# SPLIT SNP
+gatk --java-options '-Xmx25g' SelectVariants \
+ -R ${ref} \
+ -V ${mergevcf} \
+ -select-type SNP \
+ -O ${workhard}/Exome_Norm_HC_calls.snps.vcf
 
-# # SPLIT INDEL
-# gatk --java-options '-Xmx25g' SelectVariants \
-#  -R ${ref} \
-#  -V ${mergevcf} \
-#  -select-type INDEL \
-#  -O ${workhard}/Exome_Norm_HC_calls.indels.vcf
+# SPLIT INDEL
+gatk --java-options '-Xmx25g' SelectVariants \
+ -R ${ref} \
+ -V ${mergevcf} \
+ -select-type INDEL \
+ -O ${workhard}/Exome_Norm_HC_calls.indels.vcf
 
-# # FILTER SNP 
-# gatk --java-options '-Xmx25g' VariantFiltration \
-#  -R ${ref} \
-#  -V ${workhard}/Exome_Norm_HC_calls.snps.vcf \
-#  --missing-values-evaluate-as-failing true \
-#  --filter-expression "QD < 2.0" \
-#  --filter-name "QD_lt_2" \
-#  --filter-expression "FS > 60.0" \
-#  --filter-name "FS_gt_60" \
-#  --filter-expression "MQ < 40.0" \
-#  --filter-name "MQ_lt_40" \
-#  --filter-expression "MQRankSum < -12.5" \
-#  --filter-name "MQRS_lt_n12.5" \
-#  --filter-expression "ReadPosRankSum < -8.0" \
-#  --filter-name "RPRS_lt_n8" \
-#  -O ${workhard}/HardFilter.snps.filtered.vcf
+# FILTER SNP 
+gatk --java-options '-Xmx25g' VariantFiltration \
+ -R ${ref} \
+ -V ${workhard}/Exome_Norm_HC_calls.snps.vcf \
+ --missing-values-evaluate-as-failing true \
+ --filter-expression "QD < 2.0" \
+ --filter-name "QD_lt_2" \
+ --filter-expression "FS > 60.0" \
+ --filter-name "FS_gt_60" \
+ --filter-expression "MQ < 40.0" \
+ --filter-name "MQ_lt_40" \
+ --filter-expression "MQRankSum < -12.5" \
+ --filter-name "MQRS_lt_n12.5" \
+ --filter-expression "ReadPosRankSum < -8.0" \
+ --filter-name "RPRS_lt_n8" \
+ -O ${workhard}/HardFilter.snps.filtered.vcf
 
-# gatk --java-options '-Xmx25g' VariantFiltration \
-#  -R ${ref} \
-#  -V ${workhard}/Exome_Norm_HC_calls.indels.vcf \
-#  --missing-values-evaluate-as-failing true \
-#  --filter-expression "QD < 2.0" \
-#  --filter-name "QD_lt_2" \
-#  --filter-expression "FS > 200.0" \
-#  --filter-name "FS_gt_200" \
-#  --filter-expression "ReadPosRankSum < -20.0" \
-#  --filter-name "RPRS_lt_n20" \
-#  -O ${workhard}/HardFilter.indels.filtered.vcf
+gatk --java-options '-Xmx25g' VariantFiltration \
+ -R ${ref} \
+ -V ${workhard}/Exome_Norm_HC_calls.indels.vcf \
+ --missing-values-evaluate-as-failing true \
+ --filter-expression "QD < 2.0" \
+ --filter-name "QD_lt_2" \
+ --filter-expression "FS > 200.0" \
+ --filter-name "FS_gt_200" \
+ --filter-expression "ReadPosRankSum < -20.0" \
+ --filter-name "RPRS_lt_n20" \
+ -O ${workhard}/HardFilter.indels.filtered.vcf
 
  # MERGE
 gatk --java-options '-Xmx25g' MergeVcfs \
