e7b6f3ff9f914310e5820dc1d90a7f73f6b06dd4
Jin0331 <M18525@hallym.ac.kr>
2021-12-09 14:51:15 +0900

    update

--- Somatic_variant_discovery/Mutect2_Merge_Sort.sh
+++ Somatic_variant_discovery/Mutect2_Merge_Sort.sh
@@ -12,10 +12,12 @@ do
         p) pon=${OPTARG};;
         i) interval=${OPTARG};;
         b) workvariant=${OPTARG};;
-        o) finalPath=${OPTARG};;
+        o) workVCF=${OPTARG};;
     esac
 done
     
+mkdir -p ${workVCF}
+
 ## Run MuTect2 using only tumor sample on chromosome level (25 commands with different intervals)    
 for mapFile in ${bamFolder}/*_final.bam
 do
@@ -24,24 +26,29 @@ do
 
     filename=$(basename $mapFile _bwa_final.bam)
     output=${filename}.mt2_${i}.vcf
+    flr2_output=${filename}.flr2_${i}.tar.gz
 
     # file list
     if [ ${i} = 1 ]
     then
-        echo ${workvariant}/${output} > ${workvariant}/${filename}_m2_vcf_file.list
+        echo ${workVCF}/${output} > ${workVCF}/${filename}_m2_vcf_file.list
+
     else
-        echo ${workvariant}/${output} >> ${workvariant}/${filename}_m2_vcf_file.list
+        echo ${workVCF}/${output} >> ${workVCF}/${filename}_m2_vcf_file.list
     fi
 
-    gatk --java-options "-Xmx5G -XX:+UseParallelGC -XX:ParallelGCThreads=5" Mutect2 \
+    gatk --java-options "-Xmx33G -XX:ConcGCThreads=1" Mutect2 \
         -R ${ref} \
         -L ${interval}/${i}-scattered.interval_list \
         -I ${mapFile} \
+        --native-pair-hmm-threads 2 \
         -tumor ${workvariant}/${filename}.targeted_sequencing.sample_name \
         --af-of-alleles-not-in-resource 2.5e-06 \
         --germline-resource ${gnomad} \
+        --f1r2-tar-gz ${workVCF}/${flr2_output} \
         -pon ${pon} \
-        -O ${workvariant}/${output} &
+        --tmp-dir /root \
+        -O ${workVCF}/${output} &
     done
     wait
 
@@ -53,20 +60,33 @@ do
     # Merge
     gatk --java-options "-Xmx20G" GatherVcfs \
             -R ${ref} \
-            -I ${workvariant}/${filename}_m2_vcf_file.list \
-            -O ${workvariant}/${combine}
+            -I ${workVCF}/${filename}_m2_vcf_file.list \
+            -O ${workVCF}/${combine}
 
     # Sort
     gatk --java-options "-Xmx20G" SortVcf \
             --SEQUENCE_DICTIONARY ${refDict} \
             --CREATE_INDEX true \
-            -I ${workvariant}/${combine} \
-            -O ${workvariant}/${sort}
+            -I ${workVCF}/${combine} \
+            -O ${workVCF}/${sort}
+
+
+    # Stats Merge
+    statslist=$(for f in ${workVCF}/${filename}*.mt2_*.vcf.stats; do echo -n "--stats $f " ;done)
+    gatk MergeMutectStats \
+        ${statslist} \
+        -O ${workVCF}/${sort}.stats
+
+    # Flr2 Merge
+    flr2list=$(for f in ${workVCF}/${filename}*.flr2_*.tar.gz; do echo -n "-I $f " ;done)
+    gatk LearnReadOrientationModel \
+        ${flr2list} \
+        -O ${workVCF}/${filename}_read-orientation-model.tar.gz
+
 
 done
-
 
 # Time stemp
 END=$(date +%s)
 DIFF=$(( $END - $START ))
-echo "Mutect2 Merge Sort $DIFF seconds"
+echo "Mutect2 Merge $DIFF seconds"
