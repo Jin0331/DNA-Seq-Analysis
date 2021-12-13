383035bdc46b311adfb784b0f563931cdf1e736d
Jin0331 <M18525@hallym.ac.kr>
2021-12-13 12:16:37 +0900

    update

--- Somatic_variant_discovery/Filter_variant.sh
+++ Somatic_variant_discovery/Filter_variant.sh
@@ -2,16 +2,15 @@
 
 START=$(date +%s)
 
-while getopts v:c:r:d:i:b:i:o: flag
+while getopts v:c:r:d:i:b:o: flag
 do
     case "${flag}" in
         v) variantFolder=${OPTARG};;
         c) vcfFolder=${OPTARG};;
         r) ref=${OPTARG};;
         d) refDict=${OPTARG};;
-        i) interval=${OPTARG};;
+        i) intervalList=${OPTARG};;
         b) workvariant=${OPTARG};;
-        i) interval=${OPTARG};;
         o) finalPath=${OPTARG};;
     esac
 done
@@ -22,49 +21,26 @@ mkdir -p ${finalPath}
 # Filter variant calls from MuTect
 for mapFile in ${vcfFolder}/*.mt2_merged_sort.vcf
 do
-    for i in `seq -f %04g 0 14`
-    do  
-        filename=$(basename $mapFile .mt2_merged_sort.vcf)
-        output=${filename}.mt2_contFiltered_${i}.vcf
 
-        # file list
-        if [ ${i} = 1 ]
-        then
-            echo ${workvariant}/${output} > ${workvariant}/${filename}_m2_contFiltered_file.list
-
-        else
-            echo ${workvariant}/${output} >> ${workvariant}/${filename}_m2_contFiltered_file.list
-        fi
-
-        gatk --java-options "-Xmx5G -XX:+UseParallelGC -XX:ParallelGCThreads=5" FilterMutectCalls \
-            -R ${ref} \
-            -V ${mapFile} \
-            -L ${interval}/${i}-scattered.interval_list \
-            --contamination-table ${variantFolder}/${filename}_targeted_sequencing.contamination_${i}.table \
-            --ob-priors ${vcfFolder}/${filename}_read-orientation-model.tar.gz \
-            -O ${workvariant}/${output} &
-    done
-    wait
-
-    # # merge scattered phenotype vcf files & filter
     filename=$(basename $mapFile .mt2_merged_sort.vcf)
-    combine=${filename}.mt2_filtered.vcf
+    output=${filename}.mt2_contFiltered.vcf
     sort=${filename}.mt2_filtered_sort.vcf
 
-
-    # Merge
-    gatk --java-options "-Xmx20G" GatherVcfs \
-            -R ${ref} \
-            -I ${workvariant}/${filename}_m2_contFiltered_file.list \
-            -O ${finalPath}/${combine}
+    # Filter Mutect Calls
+    gatk --java-options "-Xmx30G" FilterMutectCalls \
+        -R ${ref} \
+        -V ${mapFile} \
+        -L ${intervalList} \
+        --contamination-table ${variantFolder}/${filename}_targeted_sequencing.contamination.table \
+        --ob-priors ${vcfFolder}/${filename}_read-orientation-model.tar.gz \
+        -O ${workvariant}/${output}
 
     # Sort
     gatk --java-options "-Xmx20G" SortVcf \
-            --SEQUENCE_DICTIONARY ${refDict} \
-            --CREATE_INDEX true \
-            -I ${finalPath}/${combine} \
-            -O ${finalPath}/${sort}
-
+        -I ${workvariant}/${output} \
+        --SEQUENCE_DICTIONARY ${refDict} \
+        --CREATE_INDEX true \
+        -O ${finalPath}/${sort}
 done
 
 # Time stemp
