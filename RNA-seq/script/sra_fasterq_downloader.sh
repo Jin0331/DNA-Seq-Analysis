2772cbe34005184ec39e6d4dd2487d38468f9a1f
Jin0331 <jinoo@wmbio.co>
2022-10-18 13:13:08 +0900

    path 수정

--- RNA-seq/script/sra_fasterq_downloader.sh
+++ RNA-seq/script/sra_fasterq_downloader.sh
@@ -3,18 +3,18 @@
 while getopts b:f:i: flag
 do
     case "${flag}" in
-    	b) basedir=${OPTARG};;
+        b) basedir=${OPTARG};;
         f) foldername=${OPTARG};;
-		i) input=${OPTARG};;
+        i) input=${OPTARG};;
     esac
 done
  
 while IFS= read -r line || [ -n "$line" ]; do
 
     echo $line
-   	docker run -t --rm -v ${basedir}/${foldername}:/output:rw -w /output ncbi/sra-tools fasterq-dump -e 15 -p --split-files ${line}
+    docker run -t --rm -v ${basedir}/${foldername}:/output:rw -w /output ncbi/sra-tools fasterq-dump -e 15 -p --split-files ${line}
 
 done < $input
 
 # # file rename
-python ${basedir}/RNA_script/script/ena_file_rename.py ${basedir}/${foldername}
+python ${basedir}/script/ena_file_rename.py ${basedir}/${foldername}
